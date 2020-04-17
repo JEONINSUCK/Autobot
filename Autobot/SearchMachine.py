15f9b94601f341adb4c580bd486fafe55f02ef49
JEONINSUCK <30279985+JEONINSUCK@users.noreply.github.com>
2020-04-17 15:32:39 +0900

    SearchMacine() Class v1.0.0

--- Autobot/SearchMachine.py
+++ Autobot/SearchMachine.py
@@ -4,21 +4,22 @@ from bs4 import BeautifulSoup
 from urllib.parse import quote, urlparse
 from urllib import parse
 import threading
+import time
 
 TARGET_URL = "https://www.coindeskkorea.com/"
 SEARCH_PATH = "news/articleList.html?sc_word="
 PAGE_PATH = "&page="
 DAY_PATH = "&sc_day="
 
-class html_parser():
-    def __init__(self,keyword="비트코인",day=80,search=1,debug=1):
+class SearchMachine():
+    def __init__(self,keyword,day=0,search=1,debug=1):
         self.is_search = search
         if day: self.is_day = 1
         else: self.day_set = 0 
         self.debug = debug
         
         self.keyword = keyword
-        self.title_link_dicts= {}
+        self.tit_lin_day_dict= {}
         self.page_num = 0
         self.day = day
 
@@ -29,42 +30,46 @@ class html_parser():
         if self.is_search:
             self.url = self.UrlSet(keyword=self.keyword, day=self.day, page=self.page_num+1)
         
-        self.PageParser(self.url)
+        self.Art_Tit_Parser(self.url)
         self.page_num = self.PageNumSet()
         
         if self.page_num > 1:
             for i in range(2, self.page_num+1):
                 self.url = self.UrlSet(keyword=self.keyword, day=self.day, page=i)
                 thread_name = "t" + str(i)
-                thread_name = threading.Thread(target=self.PageParser, args=(self.url,))
+                thread_name = threading.Thread(target=self.Art_Tit_Parser, args=(self.url,))
                 thread_name.start()
                 self.thread_name_lists.append(thread_name)
 
             for self.thread_name_list in self.thread_name_lists:
                 self.thread_name_list.join()
 
-        print(len(self.title_link_dicts))
+        if self.debug:
+            print("article num: ", len(self.tit_lin_day_dict))
+            for i in range(1, len(self.tit_lin_day_dict)+1):
+                print("article: ", self.tit_lin_day_dict[i]['article'])
+                print("url: ", self.tit_lin_day_dict[i]['url'])
+                print("day: ", self.tit_lin_day_dict[i]['day'])
 
     def PageNumSet(self):
         url = self.soup.find("li","pagination-end").find("a").attrs['href']
         parse_data = parse.parse_qs(urlparse(url).query)
         page_num = int((parse_data['page'][0]))
-        if self.debug:
-            print(page_num)
         return page_num
 
-    def PageParser(self, url):
+    def Art_Tit_Parser(self, url):
         with urllib.request.urlopen(url) as self.response:
                 self.soup = BeautifulSoup(self.response.read(), "html.parser")
-                self.first_divs = self.soup.select("a.links")
+                self.first_divs = self.soup.select("div.table-row")
                 for self.first_div in self.first_divs:
                     self.article_num += 1
-                    if(self.first_div.find("strong")):
-                        self.title_link_dicts.update({self.article_num:
-                                                                        {"article": self.first_div.text,
-                                                                        "url" : self.first_div.attrs['href']}
+                    if self.first_div.find("strong"):
+                        self.art_day = self.first_div.find("div", "list-dated").get_text()
+                        self.tit_lin_day_dict.update({self.article_num: 
+                                                                        {"article" : self.first_div.find("a", "links").get_text(),
+                                                                        "url" : self.first_div.find("a", "links").attrs['href'],
+                                                                        "day" : self.art_day}
                                                                         })
-                    if self.debug:  print(self.first_div.text)                    
 
     def UrlSet(self, keyword=None, day=None, page=None, path=None):
         if keyword:
@@ -73,6 +78,8 @@ class html_parser():
             url_set = TARGET_URL + path
         return url_set
 
+    def Art_Day_Parser(self, data):
+        return data.split("|")[1]
 
     def UrlParser(self):
         pass
@@ -80,5 +87,7 @@ class html_parser():
 
 
 if __name__ == "__main__":
-    test = html_parser()
+    start = time.time()
+    test = SearchMachine("비트코인", day=7, debug=1)
     test.Run()
+    print("time: {0} 초".format(time.time() - start))
--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import requests
 import json
 
-base_url = "https://www.coindeskkorea.com/news/articleList.html"
-data = {"input":"bitcoin"}
-r = requests.post(base_url,data=json.dumps(data))
-print(r.text)
+data = "Wolfie Zhao | 2020-04-16 10:00"
+
+print(data.split("|")[1])
