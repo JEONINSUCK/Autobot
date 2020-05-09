db3075f54549a9ddabdabc7e7ef54cd50735055f
JEONINSUCK <30279985+JEONINSUCK@users.noreply.github.com>
2020-05-09 19:44:55 +0900

    Path() class added

--- TradingBot/BitHumb/OpenAPI.py
+++ TradingBot/BitHumb/OpenAPI.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import os
 import pybithumb
 from xcoin_api_client import *
 
@@ -42,7 +43,7 @@ class Public():
             "fluctate_rate_24H": 0.19,
             "date": "1417141032622"
     }
-}
+    }
         """
         try:
             url = DEFUALTPATH + TICKERPATH + "/{0}_{1}".format(order_currency, payment_currency)
@@ -85,7 +86,7 @@ class Public():
             {
                 "quantity"  : "3.54343",
                 "price"     : "507000"    }
-}
+    }
         
         """
         try:
@@ -130,7 +131,7 @@ class Public():
             "total"             : "758"
         }
     ]
-}
+    }
         """
         try:
             url = DEFUALTPATH + TRANSHISPATH + "/{0}_{1}?count={2}".format(order_currency, payment_currency,count)
@@ -163,7 +164,7 @@ class Public():
                   },
         "date" : "1542002781568"
     }
-}
+    }
         """
         try:
             url = DEFUALTPATH + BTCIPATH
@@ -172,15 +173,41 @@ class Public():
         except Exception as e:
             return -1
 
+class Path():
+    def __init__(self):
+        self.top_path = os.getcwd()
+        self.default_path = __file__
+        self.key_path = "/key.lock"
+
+    def KeyPath(self):
+        dirpath = self.GetDir(self.default_path)
+        if(self.CheckFile(dirpath + self.key_path)):
+            return dirpath + self.key_path
+    
+    def CheckDir(self, path):
+        return os.path.isdir(path)
+
+    def GetDir(self, path):
+        return os.path.dirname(path)
+
+    def GetDirList(self, path):
+        return os.listdir(path)    
         
-    
-    
+    def CheckFile(self, path):
+        return os.path.exists(path)
 
 
 if __name__ == "__main__":
     start = time.time()
-    connect = "1168d95f1a7c1d1166e421f7b31d3f8f"
-    secret = "1c9aaede2ab3ba4ce6b51cb8e4894dea"
+
+    with open(Path().KeyPath(), "r") as f:
+        buf = f.read().split()
+        connect = buf[1]
+        secret = buf[3]
+    print(connect)
+    print(secret)
+
+    # print(Path().KeyPath())
     
     # print(Public().Ticker("BTC"))
     # print(Public().OrderBook("BTC"))
