e5c969d5af6b433a5ef128cdc5004ead4b3475cd
Fhwang <hdh0926@naver.com>
2020-05-17 02:33:58 +0900

    reinit

--- Crawler_v1/Lib_v1/Rss.py
+++ Crawler_v1/Lib_v1/Rss.py
@@ -1,72 +1,80 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 # auth : hdh0926@naver.com
+
 import datetime
 import time
 import feedparser
 from pprint import pprint
 from Protocol import news, news_collection
 from threading import Thread
-import json
+import json, sys
 
-class RssService(Thread):
-  debug   = False
-  url  = ""
-  last    = ""
-
-  def __init__(self, debug = False, target = "", last = ""):
-    super().__init__(self, target, last)
-    self.debug = debug
-    self.url = target
-    self.last = last
-
-  def run(self):
-    print("{} started!".format(self.getName()))        # "Thread-x started!"
-    start = time.time()
-    rss = feedparser.parse(self.url)
-
-    # start parser
-    newsList = []
-    try:
-      if self.last == "":
-        for row in rss["entries"]:
-          _news = news( title = row["title"], content = ''.join(row["summary"].split("\n")), published = row["published"] )
-          # if self.debug : print(_news.published)
-          newsList.append(_news)
-        self.last = timestamp = time.mktime(datetime.datetime.now().timetuple()) 
-      else:
-        for row in rss["entries"]:
-          _news = news( title = row["title"], content = ''.join(row["summary"].split("\n")), published = row["published"] )
-          if self.last > published:
-            continue
-          if self.debug : print("[+] new " + _news.published)
-          newsList.append(_news)
-          self.last = time.mktime(datetime.strptime(_news.published, '%y-%m-%d %H:%m:%S').timetuple)
-
-      print("[+] news count : {}".format(len(newsList)))
-      print("[+] only run function time: {0}".format(time.time() - start))
-      print("[+] {}".format(self.last))
-    except Exception as e:
-      print(e)
+# start schedule
+# 스케줄 종류에는 여러가지가 있는데 대표적으로 BlockingScheduler, BackgroundScheduler 입니다
+# BlockingScheduler 는 단일수행에, BackgroundScheduler은 다수 수행에 사용됩니다.
+# 여기서는 BackgroundScheduler 를 사용하겠습니다.
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.base import JobLookupError
+import hashlib, queue
 
 class RssWatcher(object):
   # define
   debug = False
-  th_list = []
-  rs_data = []
+  sched = BackgroundScheduler()
+  jobs_list = {}
+  q = queue.Queue()
 
   def __init__(self, debug = False):
+    super().__init__()
     self.debug = debug
   
-  # all wait
-  def wait(self):
-    for th in self.th_list:
-      th.join()
-  
-  # all start
-  def start(self):
-    for th in self.th_list:
-      th.start()
+  def LastParser(self, row):
+    try:
+      updated_at = row["published"]
+      last = time.mktime(datetime.datetime.strptime(updated_at, '%Y-%m-%d %H:%M:%S').timetuple())
+      return last
+    except Exception as e:
+      print(e)
+      return 0
+
+  def run(self, target, last, key):
+    start = time.time()
+    rss = feedparser.parse(target)
+    last_time = last.strip()
+    print("insert", last_time)
+    if not last_time:
+      last_time = 0
+    if self.jobs_list[key]["last"]:
+      last_time = self.jobs_list[key]["last"]
+      print("update", last_time, key)
+
+    # start parser
+    newsList = []
+    try:
+      if last_time <= 0 :
+        # new init
+        for row in rss["entries"]:
+          _news = news( title = row["title"], content = ''.join(row["summary"].split("\n")), published = self.LastParser(row) )
+          newsList.append(_news)
+      else:
+        # update init
+        for row in rss["entries"]:
+          _news = news( title = row["title"], content = ''.join(row["summary"].split("\n")), published = self.LastParser(row) )
+          newsList.append(_news)
+          a = self.LastParser(row)
+          print(a, "asdasda")
+          if last_time > a:
+            continue
+      # last action
+      print(newsList[len(newsList) - 1].published)
+      self.jobs_list[key]["last"] = newsList[len(newsList) - 1].published
+      print("[+] Last : {0}, Count : {1}".format(self.jobs_list[key]["last"], str(len(newsList))))
+      print("[+] Runtime : {0}".format(str(time.time() - start)))
+      # pprint(self.jobs_list[key])
+      self.q.put({ "newsList" : newsList, "target" : target, "key" : key })
+    except Exception as e:
+      print(e)
   
   def runner(self):
     try:
@@ -75,34 +83,46 @@ class RssWatcher(object):
         config = json.load(json_file)
 
       # check config data
-      if(config == ""):
-        print("[-] not found config file")
+      if not config:
+        print("[ERROR] No have value on config file, "+config)
         sys.exit()
-        
+      
+      # start schedule on background
+      self.sched.start()
+
       # read data file
       r = open("data/rss_kr.txt", mode='r', encoding='utf-8')
       for x in r.readlines():
-        if(x.startswith("#")):
-          print("[DEBUG]"+x)
+        if not x.startswith("http"):
+          if self.debug:
+            print("[DEBUG][PASS] "+x)
+          continue
         else:
-          print("[INFO][RUN]"+x)
-          target, interval = x.split(",")
-          th = RssWatcher(target = target, interval = interval, debug = True)
-          self.th_list.append(th)
-        # start wait
-        print("[+] load count : {0}".format(self.th_list))
-        self.start()
-        self.wait()
+          # add schedule
+          target, interval, last = x.split(",")
+          key = hashlib.md5(target.encode('utf-8')).hexdigest()
+          params = { "target" : target, "last" : last, "key" : key }
+          self.sched.add_job(self.run, 'interval', seconds=int(interval), id=key, kwargs=params)
+          self.jobs_list[key] = params
+          print(self.jobs_list[key])
+
+          # logging
+          if self.debug:
+            print("[INFO][RUN] "+x)
+      # start wait
+      # self.th_start()
+      # self.th_wait()
     except Exception as e:
-      print("[-] Error: unable to start thread", e)
-    while 1:
+      print("[ERROR] unable to start thread", e)
+    while self.sched.running:
       print("run main")
+      if not self.q.empty:
+        pprint(self.q.get())
       time.sleep(1)
       pass
 
 if __name__ == "__main__":
     start = time.time()
-    target = "http://www.coindeskkorea.com/rss/allArticle.xml"
     r_service = RssWatcher(debug = True)
     r_service.runner()
     print("time: {0}".format(time.time() - start))
