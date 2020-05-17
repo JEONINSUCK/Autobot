8da7e186c9fe72fd459957f9ce727085cdb561e7
JEONINSUCK <30279985+JEONINSUCK@users.noreply.github.com>
2020-05-17 20:17:35 +0900

    GetOrderDetail(), GetBalance(), GetTradeFee(), MarketBuy(), MarketSell() added

--- TradingBot/BitHumb/UserAPI.py
+++ TradingBot/BitHumb/UserAPI.py
@@ -17,6 +17,10 @@ class Bithumb():
         * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
         * param payment_currency: KRW(Default)
         * return type: dict
+        * return value: {order_currency: {
+                                        "시가": value,
+                                        "현재가": value,
+                                        "시간": value }})
         """
         try:
             pars_data = {}
@@ -56,6 +60,7 @@ class Bithumb():
 
         * param payment_currency: KRW(Default)
         * return type: dict
+        * return value: {value, value, ...}
         """
         try:
             resp = Public().Ticker(
@@ -67,15 +72,16 @@ class Bithumb():
         except Exception as e:
             return e
 
-    def OrderBuy(self, units, price, type=BUY, order_currency="BTG", payment_currency="KRW"):
+    def DesignBuy(self, units, price, type=BUY, order_currency="BTG", payment_currency="KRW"):
         """
-        Order to buy the coin
+        Reservate to buy the coin
 
         * Param units: coin quantity
         * Param price: coin price
         * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
         * param payment_currency: KRW(Default)
         * return type: dict
+        * return value: {order_currency: {order_id: value}}
         """
         try:
             resp = Private(self.con_key, self.secr_key).Place(
@@ -90,15 +96,16 @@ class Bithumb():
         except  Exception as e:
             return e
 
-    def OrderSell(self, units, price, type=SELL, order_currency="BTG", payment_currency="KRW"):
+    def DesignSell(self, units, price, type=SELL, order_currency="BTG", payment_currency="KRW"):
         """
-        Order to sell the coin
+        Reservate to sell the coin
 
         * Param units: coin quantity
         * Param price: coin price
         * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
         * param payment_currency: KRW(Default)
         * return type: dict
+        * return value: {order_currency: {order_id: value}}
         """
         try:
             resp = Private(self.con_key, self.secr_key).Place(
@@ -140,7 +147,8 @@ class Bithumb():
         """
         Get the order id list
 
-        * return type: list
+        * return type: dict
+        * return value: {'buy': [], 'sell': []}
         """
         try:
             resp = Private(self.con_key, self.secr_key).Order(
@@ -155,6 +163,99 @@ class Bithumb():
                 elif res['type'] == SELL:
                     self.order_id_dict["sell"].append(res['order_id'])
             return self.order_id_dict
+        except Exception as e:
+            return e
+
+    def GetOrderDetail(self, order_id, order_currency="BTG", payment_currency="KRW"):
+        """
+        Get the information of the order detaily
+
+        * Param order_id: your order number
+        * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
+        * param payment_currency: KRW(Default)
+        * return type: dict
+        * return value: {order_currency: {
+                                            "매수/매도": value,
+                                            "주문금액": value,
+                                            "주문수량": value,
+                                            "주문상태": value,
+                                            "취소날짜": value,
+                                            "주문시간": value }})
+        """
+        try:
+            pars_data = {}
+            resp = Private(self.con_key, self.secr_key).OrderDetail(
+                                                                    order_id=order_id,
+                                                                    order_currency=order_currency, 
+                                                                    payment_currency=payment_currency)
+            if 'message' in resp:
+                return resp['message']
+            resp = resp['data']
+            if(resp['type'] == "bid"):
+                ordertype = "매수"
+            else:
+                ordertype = "매도"
+            pars_data.update({order_currency: {
+                                            "매수/매도": ordertype,
+                                            "주문금액": resp['order_price'],
+                                            "주문수량": resp['order_qty'],
+                                            "주문상태": resp['order_status'],
+                                            "취소날짜": resp['cancel_date'],
+                                            "주문시간": resp['order_date']}
+                                            })
+            return pars_data
+        except Exception as e:
+            return e
+
+    def GetTransHis(self, offset=0, count=20, searchGb=0, order_currency="BTG", payment_currency="KRW"):
+        """
+        Get the transaction history
+
+        * Param count: 20(Default) 1-50 (the number of lists)
+        * Param searchGb: 0(Defualt) : 전체, 1 : 매수 완료, 2 : 매도 완료, 3 : 출금 중 4 : 입금, 5 : 출금, 9 : KRW 입금 중
+        * Param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
+        * Param payment_currency: KRW(Default)
+        * return type: dict
+        * return value: 
+        """
+        try:
+            pars_data = {order_currency: {}}
+            resp = Private(self.con_key, self.secr_key).Transaction(
+                                                                offset=offset, 
+                                                                count=count, 
+                                                                searchGb=searchGb, 
+                                                                order_currency=order_currency, 
+                                                                payment_currency=payment_currency)
+            if 'message' in resp:
+                return resp['message']
+            resp = resp['data']
+            
+            for res in resp:
+                if res['search'] == "1":
+                    search_type = "구매완료"
+                elif res['search'] == "2":
+                    search_type = "판매완료"
+                elif res['search'] == "3":
+                    search_type = "출금중"
+                elif res['search'] == "4":
+                    search_type = "입금"
+                elif res['search'] == "5":
+                    search_type = "출금"
+                elif res['search'] == "9":
+                    search_type = "KRW입금중"
+
+                pars_data[order_currency].update({
+                                res['transfer_date']:{
+                                                    "거래구분": search_type,
+                                                    "거래코인": res['order_currency'],
+                                                    "거래수량": res['units'],
+                                                    "1개당금액": res['price'],
+                                                    "거래금액": res['amount'],
+                                                    "거래수수료": res['units'],
+                                                    "잔여수량": res['order_balance'],
+                                                    "잔여금액": res['payment_balance']                            
+                 }})
+            return pars_data
         except Exception as e:
             return e
 
@@ -180,6 +281,103 @@ class Bithumb():
         except Exception as e:
             return e
 
+    def GetBalance(self, currency):
+        """
+        Get my asset
+
+        * param currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
+        * return type: dict
+        * return value: {currency: {
+                                    "보유금액": value,
+                                    "예약금액": value,
+                                    "사용가능금액": value,
+                                    "보유코인": value,
+                                    "판매가능코인": value,
+                                    "판매예약수량": value
+        }}
+        """
+        try:
+            pars_data = {}
+            resp = Private(self.con_key, self.secr_key).Balance(
+                                                                currency=currency)
+            if 'message' in resp:
+                return resp['message']
+            resp = resp['data']
+            pars_data.update({currency: 
+                            {"보유금액": resp['total_krw'],
+                            "예약금액": resp['in_use_krw'],
+                            "사용가능금액": resp['available_krw'],
+                            "보유코인": resp["total_"+currency.lower()],
+                            "판매가능코인": resp['available_'+currency.lower()],
+                            "판매예약수량": resp['in_use_'+currency.lower()]}
+            })
+            return pars_data
+        except Exception as e:
+            return e
+
+    def GetTradeFee(self, order_currency="BTG", payment_currency="KRW"):
+        """
+        Get the trade fee
+
+        * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
+        * param payment_currency: KRW(Default)
+        * return type: dict
+        * return value: { order_currency: {"거래수수료": value}}
+        """
+        try:
+            resp = Private(self.con_key, self.secr_key).Account(
+                                                            order_currency=order_currency,
+                                                            payment_currency=payment_currency)
+            if 'message' in resp:
+                return resp['message']
+            resp = resp['data']
+            return { order_currency: {"거래수수료": resp['trade_fee']}}
+        except Exception as e:
+            return e
+    
+    def MarketBuy(self, units, order_currency=DEFAULTCOIN, payment_currency=DEFAULTPAYMENT):
+        """
+        Buy the coin at market price
+
+        * Param units: coin quantity
+        * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
+        * param payment_currency: KRW(Default)
+        * return type: dict
+        * return value: {order_currency: {"order_id": value}}
+        """
+        try:
+            resp = Private(self.con_key, self.secr_key).MarketPriceBuy(
+                                                                    units=units,
+                                                                    order_currency=order_currency,
+                                                                    payment_currency=payment_currency)
+            if 'message' in resp:
+                return resp['message']
+            return {order_currency: {"order_id": resp['order_id']}}
+        except Exception as e:
+            return e
+
+    def MarketSell(self, units, order_currency=DEFAULTCOIN, payment_currency=DEFAULTPAYMENT):
+        """
+        Sell the coin at market price
+
+        * Param units: coin quantity
+        * param order_currency: BTG(Default)/BTC/ETH/DASH/LTC/ETC/XRP/BCH/XMR/ZEC/QTUM/EOS/ICX/VEN/TRX/ELF/MITH/MCO/OMG/KNC
+        * param payment_currency: KRW(Default)
+        * return type: dict
+        * return value: {order_currency: {"order_id": value}}
+        """
+        try:
+            resp = Private(self.con_key, self.secr_key).MarketPriceSell(
+                                                                    units=units,
+                                                                    order_currency=order_currency,
+                                                                    payment_currency=payment_currency)
+            if 'message' in resp:
+                return resp['message']
+            return {order_currency: {"order_id": resp['order_id']}}
+        except Exception as e:
+            return e
+
+
 if __name__ == "__main__":
     start = time.time()
     with open(Path().KeyPath(), "r") as f:
@@ -190,14 +388,17 @@ if __name__ == "__main__":
     test = Bithumb(connect, secret)
     # print(test.GetPrice())
     # print(test.GetTicker())
-    # print(test.OrderBuy(units=0.2, price=10800))
-    # print(test.OrderSell(units=1, price=10800))
+    # print(test.DesignBuy(units=0.1, price=10800))
+    # print(test.DesignSell(units=0.1, price=11000))
     # print(test.GetOrderID())
     # print(test.OrderCancel(type=BUY, order_id=))
-    print(test.CancelAll())
-    
-
-
+    # print(test.CancelAll())
+    # print(test.GetBalance(currency="BTG"))
+    # print(test.GetTradeFee(order_currency="BTG"))
+    # print(test.GetOrderDetail(test.GetOrderID()['buy'][0]))
+    # print(test.GetTransHis())
+    # print(test.MarketBuy(units=0.1))
+    # print(test.MarketSell(units=0.6))
 
     print("\ntime: {} 초".format(time.time() - start))
     # print(time.gmtime(time.time()))
