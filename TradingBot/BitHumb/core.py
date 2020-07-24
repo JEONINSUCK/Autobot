0300598799b3bc11a8dafbd9b057f68297b19416
JEONINSUCK <30279985+JEONINSUCK@users.noreply.github.com>
2020-07-24 17:43:07 +0900

    StoreMA() added

--- TradingBot/BitHumb/core.py
+++ TradingBot/BitHumb/core.py
@@ -1,19 +1,86 @@
 import UserAPI
 import DBHandler
 import threading
+import time
+from enum import Enum
+from pandas import DataFrame
+
+
+class Candle(Enum):
+    date = 0
+    ticktype = 1
+    time = 2
+    openprice = 3
+    closeprice = 4
+    lowprice = 5
+    highprice = 6
+    symbol = 7
+
+COINTYPE = "BTC"
+TICKTYPE = "30M"
+DBHOST = "localhost"
+DBUSER = 'root'
+DBPASSWORD = 'root'
+DBNAME = 'test'
+CANTABLE = "candle_test"
+TRANTABLE = "trans_test"
+MATABLE = "ma_test"
+DBCOLUMN = "*"
+
+def GetMA(data):
+    """
+    Store the MA(moving average) data
+    """
+    try:
+        pass
+    except Exception as e:
+        return e
+
+def StoreMA(num):
+    """
+    Return the MA(Moving average) data
+    """
+    try:
+        dbhand = DBHandler.DBHandler(host=DBHOST, user=DBUSER, password=DBPASSWORD, db=DBNAME)
+        date = time.localtime()
+        date = "%02d%02d%02d" %(date.tm_year, date.tm_mon, date.tm_mday)
+        options = "where date={0}\
+                        order by time desc\
+                        limit {1}".format(date, num)
+        candle_datas = dbhand.DbGet(select=DBCOLUMN, ffrom=CANTABLE, options=options)
+        if candle_datas == -1:
+            return candle_datas
+        if candle_datas == 0:
+            return candle_datas
+        standard_time = candle_datas[0][Candle.time.value]
+        ma_value = 0
+        for candle_data in candle_datas:
+            ma_value += int(candle_data[Candle.closeprice.value])
+        ma_value = ma_value / num
+        df = DataFrame(data=[[date, standard_time, ma_value, num]], columns=['date', 'time', 'maValue', 'maNum'])
+        df = df.set_index('date')
+        res = dbhand.DbSend(df, MATABLE)
+        if res == -1: 
+            print("table is not exist")
+        return res
+    except Exception as e:
+        return e
 
 def StoreCandle(con,sec):
+    """
+    Store the candle stick data into database
+    """
     try:
-        dbhand2 = DBHandler.DBHandler(host="localhost", user='root', password='root', db='test')
+        dbhand = DBHandler.DBHandler(host=DBHOST, user=DBUSER, password=DBPASSWORD, db=DBNAME)
         cbit = UserAPI.Bithumb(con, sec)
-        res = cbit.CandleObsStart(order_currency="BTC", tickTypes="30M")
+        res = cbit.CandleObsStart(order_currency=COINTYPE, tickTypes=TICKTYPE)
         while True:
             data = cbit.NowCandleStick()
             if isinstance(data, int):
                 print("observer is not runing")
                 break
             print(data)
-            res = dbhand2.Hander(data, "candle_test")
+            res = dbhand.DbSend(data, CANTABLE)
             if res == -1: 
                 print("table is not exist")
                 break
@@ -23,17 +90,20 @@ def StoreCandle(con,sec):
         cbit.CandleObsStop()
 
 def StoreTrans(con,sec):
+    """
+    Store the transaciton data into the database
+    """
     try:
-        dbhand1 = DBHandler.DBHandler(host="localhost", user='root', password='root', db='test')
+        dbhand = DBHandler.DBHandler(host=DBHOST, user=DBUSER, password=DBPASSWORD, db=DBNAME)
         tbit = UserAPI.Bithumb(con, sec)
-        res = tbit.TransObsStart(order_currency="BTC")
+        res = tbit.TransObsStart(order_currency=COINTYPE)
         while True:
             data = tbit.NowTransaction()
             if isinstance(data, int):
                 print("observer is not runing")
                 break
             print(data)
-            res = dbhand1.Hander(data, "trans_test")
+            res = dbhand.DbSend(data, TRANTABLE)
             if res == -1: 
                 print("table is not exist")
                 break
@@ -59,13 +129,15 @@ if __name__ == "__main__":
         bithumb = UserAPI.Bithumb(conkey, seckey)
         
 
-        candle_th = threading.Thread(target=StoreCandle, args=(conkey,seckey))
-        candle_th.start()
-        trans_th = threading.Thread(target=StoreTrans, args=(conkey,seckey))
-        trans_th.start()
+        # candle_th = threading.Thread(target=StoreCandle, args=(conkey,seckey))
+        # candle_th.start()
+        # trans_th = threading.Thread(target=StoreTrans, args=(conkey,seckey))
+        # trans_th.start()
 
-        candle_th.join()
-        trans_th.join()
+        # candle_th.join()
+        # trans_th.join()
+
+        print(StoreMA(num=20))
     except Exception as e:
         print(e)
     
